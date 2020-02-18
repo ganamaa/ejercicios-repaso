46317bc925692266775f9cd0801ee444e7433dd9
ganamaa <54151757+ganamaa@users.noreply.github.com>
2020-02-17 23:55:32 -0500

    Create transparenciaNo.py

---
+++ transparenciaNo.py
@@ -0,0 +1,7 @@
+def iva():
+    iva = 12
+    costo = int (input('¿Cual es el monto a calcular?: ')
+    calculo = costo * iva / 100
+    print("El calculo de IVA es: " + str(calculo) + "\n")
+    
+iva()
